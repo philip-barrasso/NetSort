962d61eaa87e1372025c3d8a51efe0899a8a1ca9
Philip Barrasso <philip.barrasso@gmail.com>
2017-06-15 21:29:57 -0400

    adding method to sort key validator interface for custom delimiters

--- src/NetSort/Validation/ISortKeyValidator.cs
+++ src/NetSort/Validation/ISortKeyValidator.cs
@@ -9,5 +9,13 @@ namespace NetSort.Validation
         /// <typeparam name="T">The type being validated</typeparam>
         /// <param name="key">The 'SortKey' value of the 'SortableAttribute'<param>
         bool IsKeyValid<T>(string key) where T : class;
+
+        /// <summary>
+        /// Implementations should check that the 'key' passed in is valid for the type param T
+        /// </summary>
+        /// <typeparam name="T">The type being validated</typeparam>
+        /// <param name="key">The 'SortKey' value of the 'SortableAttribute'<param>
+        /// <param name="delimiter">The char used to separate nested properties</param>
+        bool IsKeyValid<T>(string key, char delimiter) where T : class;
     }
 }
